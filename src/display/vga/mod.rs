b1b1769984f7a505cd2c08ffda3c6671cb52b01a
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2022-04-15 23:50:32 +0200

    Chore(Test): Add Tests For VGA Buffer

--- src/display/vga/mod.rs
+++ src/display/vga/mod.rs
@@ -53,3 +53,61 @@ macro_rules! eprintln {
         $crate::eprint!("{}\n", format_args!($($arg)*));
     })
 }
+
+#[cfg(test)]
+mod test {
+    mod unit {
+        use crate::display::vga::{buffer::BUFFER_HEIGHT, VGA_DISPLAY};
+
+        #[test_case]
+        fn test_println_doesnt_panic() {
+            for _ in 0..200 {
+                println!("printing text to screen!");
+            }
+        }
+
+        #[test_case]
+        fn test_println_output_is_correct() {
+            let s = "Some very random string on a single line";
+            println!("{}", s);
+
+            for (i, c) in s.chars().enumerate() {
+                let colored_char = VGA_DISPLAY.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+                assert_eq!(char::from(colored_char.ascii_character), c);
+            }
+        }
+
+        #[test_case]
+        fn test_println_wraps_text_longer_than_screen_width() {
+            let s = "Some very random text that exceeds the total length of characters expected for a single line on a VGA buffer screen";
+            println!("{}", s);
+
+            let (line_3, line_2) = s.split_at(80);
+            for (column, t_char) in line_3.chars().enumerate() {
+                let c_char = VGA_DISPLAY.lock().buffer.chars[BUFFER_HEIGHT - 3][column].read();
+                assert_eq!(char::from(c_char.ascii_character), t_char);
+            }
+
+            for (column, t_char) in line_2.chars().enumerate() {
+                let c_char = VGA_DISPLAY.lock().buffer.chars[BUFFER_HEIGHT - 2][column].read();
+                assert_eq!(char::from(c_char.ascii_character), t_char);
+            }
+        }
+
+        #[test_case]
+        fn test_new_lines() {
+            let texts = ["line A", "line B", "line C"];
+            for text in texts {
+                println!("{}", text);
+            }
+
+            for (line, text) in texts.iter().rev().enumerate() {
+                for (column, t_char) in text.chars().enumerate() {
+                    let c_char =
+                        VGA_DISPLAY.lock().buffer.chars[BUFFER_HEIGHT - line - 2][column].read();
+                    assert_eq!(char::from(c_char.ascii_character), t_char);
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::testing::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod display;
+mod exit;
 mod serial;
+#[cfg(test)]
+mod testing;
+
 use self::display::vga;
-use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Dispay up!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
     eprintln!("{}", info);
     loop {}
 }
